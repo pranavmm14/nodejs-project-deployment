9ca01ba3626fae04627d26f595427ded553b4395
Pranav Mehendale <pranavmehe14@gmail.com>
2025-01-02 15:57:10 +0530

    Added name

--- helloWorld.cjs
+++ helloWorld.cjs
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 var test;
-app.all('/', (req, res) => res.send(test));
+app.all('/', (req, res) => res.send("Pranav"));
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
